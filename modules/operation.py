0a213c47a54f3ecee3e2a874379078c4465511f7
BuildTools <unconfigured@null.spigotmc.org>
2023-03-23 08:54:42 +0100

    Stuff

--- modules/operation.py
+++ modules/operation.py
@@ -1,11 +1,23 @@
-import requests
-import json
+import os
 from datetime import date
-import re
+import csv, sqlite3
+import pandas as pd
 
-def initialGet(ddate):
-    res = requests.get('https://archive.sensor.community/'+str(ddate.year)+'-'+f"{ddate.month:02d}"+'-'+f"{ddate.day-1:02d}"+'/')
-    #res = requests.get('https://archive.sensor.community/2023-02-11/')
-    print(re.search("<a href=\"(.+?)\">",str(res.content)))
+csv_files = "./sqlite/database/sensor_data/"
 
-initialGet(date.today())
+con = sqlite3.connect("./sqlite/database/database.db") # change to 'sqlite:///your_filename.db'
+cur = con.cursor()
+#cur.execute("CREATE TABLE t (col1, col2);") # use your column names here
+
+def initialize():
+    cur.execute("CREATE TABLE IF NOT EXIST Entries (sensor_id INT,sensor_type TEXT,location INT,lat REAL,lon REAL,timestamp TEXT,P1 REAL,durP1 REAL,ratioP1 REAL,P2 REAL,durP2 REAL,ratioP2 REAL)")
+    for path in os.listdir(csv_files):
+        if os.path.isfile(os.path.join(csv_files, path)):
+            users = pd.read_csv(path)
+            users.to_sql('users', con, if_exists='append', index = False)
+            print(path)
+
+def test(results):
+    print(results)
+
+initialize()
--- modules/sql.py
+++ modules/sql.py
@@ -1,5 +1,9 @@
 import sqlite3
-con = sqlite3.connect("./sqlite/database/sensor.db")
+con = sqlite3.connect("./sqlite/database/database.db")
 
 cur = con.cursor()
-cur.execute("SELECT * FROM sensor")
+#cur.execute("SELECT * FROM sensor")
+
+def execute(query,params,callback):
+    exe = cur.execute(query,params)
+    callback(exe.fetchall())
